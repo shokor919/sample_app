2ec89e3a6b25a38dda5ad852b39e6f9830190abf
EC2 Default User <ec2-user@ip-172-31-0-127.us-east-2.compute.internal>
2018-05-05 11:55:19 +0000

    Add account activation

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,6 +33,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'b6eab338b13345108266cfa614f79183.vfs.cloud9.us-east-2.amazonaws.com' }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
