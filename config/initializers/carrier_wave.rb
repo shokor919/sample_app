81439f5f7e7e395750335804d7ddc22f4c147de5
EC2 Default User <ec2-user@ip-172-31-0-127.us-east-2.compute.internal>
2018-05-08 05:16:23 +0000

    s3

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,8 +4,8 @@ if Rails.env.production?
       # Amazon S3用の設定
       :provider              => 'AWS',
       :region                => ENV['ap-northeast-1'],     # 例: 'ap-northeast-1'
-      :aws_access_key_id     => ENV['AKIAIK7KRN33PPK5B2GQ'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['AKIAJOD5VMALV57O5MAQ'],
+      :aws_secret_access_key => ENV['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']
     }
     config.fog_directory     =  ENV['railsturorial0508']
   end
